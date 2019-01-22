3e0d2bcf9b94544b0390f64becb37d26bc1f5334
jochumg <jochum@myphp.nl>
2019-01-22 19:01:34 +0100

    complete wekende server met flask op poort 80 en python

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
+#docker build ./  -t apidemo
+#docker run -p 80:80 -t apidemo
+
 FROM tiangolo/uwsgi-nginx-flask:flask
 
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hey I'm using Docker!"
+    return "Hey I'm using Docker! Az well"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=80)
